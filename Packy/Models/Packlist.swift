d660e30809d086633182b878da495791b609b437
Philip Winsnes <philip.winsnes@icloud.com>
2021-10-28 12:14:26 +0200

    Added a Packlist struct.

---
+++ Packy/Models/Packlist.swift
@@ -0,0 +1,13 @@
+//
+//  Packlist.swift
+//  Packy
+//
+//  Created by Philip Winsnes on 2021-10-28.
+//
+
+import Foundation
+
+struct Packlist {
+    var title: String
+    var dateOfCreation: Date
+}
